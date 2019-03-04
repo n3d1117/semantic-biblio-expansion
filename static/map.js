d15ded6351d8d3166694787d393ff39480291705
ned <ned@github>
2019-03-04 13:45:40 +0100

    new improvements

--- static/map.js
+++ static/map.js
@@ -6,7 +6,13 @@ let osmUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
     layers = [],
     additionalLayers = [];
 
-let map = L.map('map', { fadeAnimation: false }).setView(toscana, 9).addLayer(osm);
+let map = L.map('map', { attributionControl: false, fadeAnimation: false }).setView(toscana, 9).addLayer(osm);
+
+let oms = new OverlappingMarkerSpiderfier(map, { keepSpiderfied: true, legWeight: 2 });
+
+oms.addListener('click', function (marker) {
+    clickZoom(marker);
+});
 
 let form = document.getElementById("search-form");
 function handleForm(event) {
@@ -19,6 +25,9 @@ L.Map.prototype.setViewOffset = function (latlng, offset) {
         targetLatLng = this.unproject(targetPoint, this.getZoom());
     return this.panTo(targetLatLng, this.getZoom());
 };
+
+let sidebar = L.control.sidebar('sidebar', { closeButton: true, position: 'right', autoPan: false });
+map.addControl(sidebar);
 
 async function searchPlaces() {
 
@@ -35,20 +44,28 @@ async function searchPlaces() {
 
             if (value.type === 'place') {
                 let marker = generateMarkerForPlace(lat, long, value);
+                marker.on('click', function() { sidebar.hide(); });
                 layers.push(marker);
+                oms.addMarker(marker);
             }
 
             // Show markers for entities too
-            if (value.type === 'entity') {
-                let marker = generateMarkerForEntity(lat, long, value);
-                layers.push(marker);
-            }
+            // if (value.type === 'entity') {
+            //     let marker = generateMarkerForEntity(lat, long, value);
+            //     layers.push(marker);
+            //     oms.addMarker(marker);
+            // }
         });
 
-        let group = L.featureGroup(layers);
-        map.fitBounds(group.getBounds(), {padding: L.point(30, 30)});
+        setTimeout(function() {
+            layers = layers.filter(function(marker) { return marker.data.hasRecords });
+            let group = L.featureGroup(layers);
+            map.fitBounds(group.getBounds(), { padding: L.point(30, 30) });
+            setTimeout(function () {
+                group.addTo(map);
+            }, 800);
+        }, 500);
 
-        setTimeout(function () { group.addTo(map) }, 500);
 
     } catch (error) {
         console.error(error);
@@ -56,34 +73,41 @@ async function searchPlaces() {
 }
 
 function generateMarkerForPlace(lat, long, place) {
-    let marker = L.marker([lat, long], { bounceOnAdd: true, bounceOnAddOptions: { duration: 500, height: 50, loop: 2} });
-    marker.data = {id: place.id, name: place.name, lat: lat, long: long};
+
+    let rDuration = getRandomInt(400, 700);
+    let rHeight = getRandomInt(50, 70);
+    let rLoop = getRandomInt(2, 4);
+
+    let marker = L.marker([lat, long], { bounceOnAdd: true, bounceOnAddOptions: { duration: rDuration, height: rHeight, loop: rLoop } });
+    marker.data = { id: place.id, name: place.name, lat: lat, long: long, hasRecords: false };
     marker.bindPopup();
-    marker.bindTooltip(place.name, {className: "marker-label", offset: [0, 0]});
-    marker.on('click', clickZoom);
+    marker.bindTooltip(place.name, { className: "marker-label" });
 
     let div = document.createElement("div");
     div.className = "landmark";
+
     let title = div.appendChild(document.createElement("h1"));
     title.textContent = place.name;
+
     let scrollable = div.appendChild(document.createElement("div"));
     scrollable.className = "scrollable";
+
     let section = scrollable.appendChild(document.createElement("section"));
 
     place.records.sort((a, b) => a.title.localeCompare(b.title)).forEach(async function(record) {
         try {
             const response = await axios.get(`/api/v1/geo_entities_for_record?record_id=${record.record_id}`);
+
             let geo_entities = response.data.filter(function removeRedundantEntity(entity) {
-                // todo better
-                return entity.title !== marker.data.name && entity.title !== 'Toscana' && entity.title !== 'Provincia di ' + marker.data.name;
+                return entity.title !== marker.data.name && entity.title !== 'Provincia di ' + marker.data.name;
             });
-            if (geo_entities.length > 0) {
-                let p = section.appendChild(document.createElement("p"));
-                p.className = "hoverable";
-                p.textContent = record.title;
-                p.onclick = function () {
-                    recordClicked(lat, long, geo_entities);
-                }
+
+            marker.data.hasRecords = geo_entities.length > 0;
+            let p = section.appendChild(document.createElement("p"));
+            p.className = "hoverable";
+            p.textContent = record.title;
+            p.onclick = function () {
+                recordClicked(lat, long, record, geo_entities);
             }
         } catch (error) {
             console.error(error);
@@ -98,44 +122,51 @@ function generateMarkerForEntity(lat, long, entity) {
     let marker = L.marker([lat, long], { icon: greenIcon, bounceOnAdd: true, bounceOnAddOptions: {duration: 1000, height: 70, loop: 3} });
     marker.bindPopup();
     marker.bindTooltip(entity.title, { direction: "right", className: "marker-label", offset: [15, -20] });
-    marker.on('click', clickZoom);
+
     let div = document.createElement("div");
     div.className = "landmark";
+
     let title = div.appendChild(document.createElement("h1"));
     title.textContent = entity.title;
+
     let scrollable = div.appendChild(document.createElement("div"));
     scrollable.className = "scrollable";
+
     if (entity.image_url !== '') {
         let img = scrollable.appendChild(document.createElement("img"));
         img.setAttribute("src", entity.image_url);
         img.setAttribute("width", "250");
         img.setAttribute("height", "100");
     }
+
     let section = scrollable.appendChild(document.createElement("section"));
+
     let coords = section.appendChild(document.createElement("div"));
     coords.className = "coords";
     let split_coords = entity.coords.split(',')
     coords.textContent = split_coords[0].substr(0, 15) + ', ' + split_coords[1].substr(0, 15);
+
     let abstract = section.appendChild(document.createElement("p"));
     abstract.textContent = entity.abstract;
+
     let uri = section.appendChild(document.createElement("p"));
     uri.className = "wikipedia-link";
     let a = uri.appendChild(document.createElement("a"));
     a.href = entity.uri;
     a.target = "_blank";
     a.textContent = "Vedi su Wikipedia →".toUpperCase();
+
     marker.setPopupContent(div);
     return marker;
 }
 
-function recordClicked(place_lat, place_long, geo_entities) {
+function recordClicked(place_lat, place_long, record, geo_entities) {
 
     // Rimuovi sognaposto entità e curve dalla mappa
     additionalLayers.map(function (c) {
         c.remove();
     });
 
-    let geo_entities_markers = [[place_lat, place_long]];
     geo_entities.map(function (entity) {
         let lat1 = Number(entity.coords.split(',')[0]);
         let long1 = Number(entity.coords.split(',')[1]);
@@ -143,18 +174,19 @@ function recordClicked(place_lat, place_long, geo_entities) {
         let marker = generateMarkerForEntity(lat1, long1, entity);
         if (!map.hasLayer(marker)) {
             marker.addTo(map);
-            geo_entities_markers.push([lat1, long1]);
+            oms.addMarker(marker);
+            additionalLayers.push(marker);
         }
-        additionalLayers.push(marker);
 
         let curve = bezierCurve([place_lat, place_long], [lat1, long1]);
         if (!map.hasLayer(curve)) {
             curve.addTo(map);
+            additionalLayers.push(curve);
         }
-        additionalLayers.push(curve);
     });
 
-    //map.fitBounds(new L.LatLngBounds(geo_entities_markers));
+    sidebar.setContent(record.title);
+    sidebar.show();
 }
 
 function clearAllLayers() {
@@ -167,6 +199,7 @@ function clearAllLayers() {
             c.remove();
         });
         additionalLayers = [];
+        sidebar.hide();
     } catch (e) {
         console.log(e.message);
     }
@@ -175,25 +208,26 @@ function clearAllLayers() {
 let greenIcon = new L.Icon({
     iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
+    iconSize: [25, 41], iconAnchor: [12, 41], popupAnchor: [1, -34], shadowSize: [41, 41]
 });
 
-function clickZoom(e) {
+function clickZoom(marker) {
     let offset = map.getZoom() <= 9 ? 120 : 90;
-    map.setViewOffset(e.target.getLatLng(), [0, offset]);
+    map.setViewOffset(marker.getLatLng(), [0, offset]);
 }
 
-/*
-map.on('popupopen', function(marker) {
-    try {
-        marker.closeTooltip();
-        console.log(marker.popup._source.data);
-    } catch(e) { }
-});
-*/
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * The value is no lower than min (or the next integer greater than min
+ * if min isn't an integer) and no greater than max (or the next integer
+ * lower than max if max isn't an integer).
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 // quadratic bezier curve
 // https://gist.github.com/ryancatalani/6091e50bf756088bf9bf5de2017b32e6
@@ -209,6 +243,6 @@ function bezierCurve(from, to) {
     let midpointX = (r2 * Math.cos(theta2)) + from[1],
         midpointY = (r2 * Math.sin(theta2)) + from[0];
     let midpointLatLng = [midpointY, midpointX];
-    let pathOptions = { color: 'black', animate: 400, weight: 1 };
+    let pathOptions = { color: 'black', animate: 600, weight: 1 };
     return L.curve(['M', from, 'Q', midpointLatLng, to], pathOptions);
 }
